ff8d433e055b8f4f83d108e4d1b1314ef8f10949
Mustafa-Mercan <mustafa.1997.38.06@gmail.com>
2023-10-04 15:39:24 +0300

    TypeScript To-Do-App Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import FavoritesPage from "./pages/FavoritesPages";
+import NavBar from "./components/NavBar";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { Flip, ToastContainer, Zoom } from "react-toastify";
 
 function App() {
   return (
-    <div className="bg-slate-500 min-h-screen text-center">
-      <p className="text-white p-20">Hello Typescript + TailwindCss</p>
+    <div className="bg-gray-400 min-h-screen">
+      <Provider store={store}>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+      <ToastContainer transition={Flip} />
     </div>
   );
 }
